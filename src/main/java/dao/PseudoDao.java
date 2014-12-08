bfbe714e9c90707376a4370250efade5809e5b67
othman853 <anuar.yasser@gmail.com>
2014-12-08 20:02:08 -0200

    Hibernate Search alterado para funcionar com Core Hibernate.

--- src/main/java/dao/PseudoDao.java
+++ src/main/java/dao/PseudoDao.java
@@ -2,32 +2,36 @@ package dao;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import model.Empresa;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.search.FullTextSession;
 import org.hibernate.search.jpa.FullTextEntityManager;
-import org.hibernate.search.jpa.Search;
+import org.hibernate.search.Search;
 import org.hibernate.search.query.dsl.QueryBuilder;
 
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.SessionScoped;
+
 @Component
 @SessionScoped
 public class PseudoDao {
 	
-	private static EntityManagerFactory factory;
-	private EntityManager manager;
+	private Session session;
+	private static SessionFactory factory;
 	
 	public PseudoDao(){
+		
 		if(factory == null){
-			factory = Persistence
-					.createEntityManagerFactory("empresa");			
+			Configuration config = new Configuration();
+			config.configure();
+			factory = config.buildSessionFactory();				
 		}
 		
-		manager = factory.createEntityManager();
+		session = factory.openSession();		
 	}
 
 	public void inserir() {
@@ -36,57 +40,60 @@ public class PseudoDao {
 		e.setNomeFantasia("Persistencia via JPA.");
 		e.setRazaoSocial("Persistência via JPA.");
 		
-		if(!manager.isOpen()){
-			manager = factory.createEntityManager();
+		if(!session.isOpen()){
+			session = factory.openSession();
 		}
 		
-		manager.getTransaction().begin();
-		manager.persist(e);
-		manager.getTransaction().commit();
+		Transaction tx = session.getTransaction();
+		tx.begin();
+		session.save(e);
+		tx.commit();
+		
 		System.out.println("Empresa inserida.");
 		System.out.println("Empresa ID: " + e.getId());
 
-		manager.close();
+		session.close();
 	}
 	
 	public void indexarDados(){
 		
-		if(!manager.isOpen()){
-			manager = factory.createEntityManager();
+		if(!session.isOpen()){
+			session = factory.openSession();
 		}
 		
-		FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(manager);
+		FullTextSession fullTextSession = Search.getFullTextSession(session);
 		try{
-			fullTextEntityManager.createIndexer().startAndWait();
+			fullTextSession.createIndexer().startAndWait();
 		}catch(InterruptedException ex){
 			System.out.println("Erro ao indexar: "+ ex.getMessage());
 		}
 		
-		manager.close();
+		session.close();
 		
 		System.out.println("Indexação completada.");
 	}
 	
 	public List<Empresa> buscar(String busca){
-		if(!manager.isOpen()){
-			manager = factory.createEntityManager();
+		if(!session.isOpen()){
+			session = factory.openSession();
 		}
 		
-		FullTextEntityManager fullTextEntityManager = org.hibernate.search.jpa.Search.getFullTextEntityManager(manager);
-		manager.getTransaction().begin();
+		FullTextSession fullTextSession = org.hibernate.search.Search.getFullTextSession(session);
+		Transaction tx = session.getTransaction();
+		tx.begin();
 		
-		QueryBuilder qb = fullTextEntityManager.getSearchFactory().buildQueryBuilder().forEntity(Empresa.class).get();
+		QueryBuilder qb = fullTextSession.getSearchFactory().buildQueryBuilder().forEntity(Empresa.class).get();
 		org.apache.lucene.search.Query luceneQuery = qb
 				.keyword()
 				.onField("nomeFantasia")
 				.matching(busca)
 				.createQuery();
-		javax.persistence.Query jpaQuery = fullTextEntityManager.createFullTextQuery(luceneQuery, Empresa.class);
+		org.hibernate.Query hibernateQuery = fullTextSession.createFullTextQuery(luceneQuery, Empresa.class);
 		
-		List<Empresa> resultadoBusca = jpaQuery.getResultList();
+		List<Empresa> resultadoBusca = hibernateQuery.list();
 				
-		manager.getTransaction().commit();
-		manager.close();
+		tx.commit();
+		session.close();
 		
 		return resultadoBusca;
 	}
